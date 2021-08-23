2e47ba9051dac74b4cff58460d2b56ef4b2869ac
Namitha Deshpande <42386287+namithadeshpande@users.noreply.github.com>
2021-08-23 17:55:47 -0500

    Create Second_Highest_Salary.sql

---
+++ Second_Highest_Salary.sql
@@ -0,0 +1,13 @@
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+
+SELECT MAX(Salary) AS SecondHighestSalary
+FROM Employee 
+WHERE Salary < (SELECT MAX(Salary) FROM Employee);
