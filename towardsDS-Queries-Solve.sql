4c93c3fcf37d94b61fa757e82aad03de5d5d7a9f
namithadeshpande <namithadeshpande@gmail.com>
2025-06-11 21:41:30 -0500

    added solutions for problem 1-5

--- towardsDS-Queries-Solve.sql
+++ towardsDS-Queries-Solve.sql
@@ -136,3 +136,11 @@ For the sample input, the output is:
 Note: If the number of students is odd, there is no need to change the last one’s seat.
 */
 
+SELECT 
+    CASE 
+        WHEN id % 2 = 1 AND id + 1 IN (SELECT id FROM SEAT) THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id 
+END AS id, student
+FROM seat 
+ORDER BY id
